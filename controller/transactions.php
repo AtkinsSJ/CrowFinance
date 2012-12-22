fdfef3c03e1e516fbc379aa3a9184546fc7121cd
Sam Atkins <github@samatkins.co.uk>
2012-12-22 13:00:47 +0000

    Transactions/View handles year, month, and day

--- controller/transactions.php
+++ controller/transactions.php
@@ -17,12 +17,24 @@ class Transactions extends Controller {
 		if (isset($dateParts[2])) { // year-month-day
 			$transactions->filter('date = :date',
 				array(
-					'startDate' => implode('-', $dateParts)
+					'date' => implode('-', $dateParts)
 			));
 		} elseif (isset($dateParts[1])) { // year-month
+			$startDate = new DateTime("{$dateParts[0]}-{$dateParts[1]}-01");
+			$endDate = clone $startDate;
+			$endDate->modify('+'.($endDate->format('t')-1).' days');
 
+			$transactions->filter('date BETWEEN :startDate AND :endDate',
+				array(
+					'startDate' => $startDate->format('Y-m-d'),
+					'endDate' => $endDate->format('Y-m-d')
+			));
 		} else { // year
-
+			$transactions->filter('date BETWEEN :startDate AND :endDate',
+				array(
+					'startDate' => "{$dateParts[0]}-01-01",
+					'endDate' => "{$dateParts[0]}-12-31",
+			));
 		}
 
 		$transactions->sortBy('date', 'ASC')->load();
